480a48d307b9ba10c563b0c01886a7b1a91aea5b
Alawode Emmanuel <alawodeemmanuel2@gmail.com>
2025-01-12 20:51:06 +0000

    fix(env strings): fixed exports and types

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,6 +1,6 @@
 import {createClient} from "@supabase/supabase-js";
 import {Database} from "../utils/database.types";
 
-const dbUrl = process.env.AUTH_DB_URL as string
-const apiSecret = process.env.AUTH_API_SECRET as string
+const dbUrl = process.env.AUTH_DB_URL || process.env.NEXT_PUBLIC_AUTH_DB_URL as string
+const apiSecret = process.env.AUTH_API_SECRET || process.env.NEXT_PUBLIC_AUTH_API_SECRET as string
 export const authClient = createClient<Database>(dbUrl, apiSecret);
